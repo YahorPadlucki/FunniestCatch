12b2a2788977547ed0a045098a8f120f63381d0e
Yahor Padlucki <yapagames@gmail.com>
2017-05-26 18:35:59 +0300

    tutorial improvements, fishes on hook - show biggest

--- js/elements/Hook.js
+++ js/elements/Hook.js
@@ -13,12 +13,12 @@ var Hook = (function () {
         this.hookSpeedX = 5;
         this.hookSpeedY = 50;
 
-        this.cameraY=0;
+        this.cameraY = 0;
 
 
         this.fishes = [];
 
-        this.device =  GameModel.getInstance().device;
+        this.device = GameModel.getInstance().device;
 
         GameModel.getInstance().doc.addEventListener(this.device.event.down, ()=> {
             if (this.goDown !== false) {
@@ -34,21 +34,21 @@ var Hook = (function () {
 
         var ctx = GameModel.getInstance().ctx;
         var diameter = 5;
-        var lineWidth =1;
+        var lineWidth = 1;
 
         ctx.beginPath();
-        ctx.rect(this.x , this.y - this.height, this.width/2, this.height);
+        ctx.rect(this.x, this.y - this.height, this.width / 2, this.height);
         ctx.fillStyle = "#ff0000";
         ctx.fill();
         ctx.closePath();
         ctx.beginPath();
-        ctx.arc(this.x  ,this.y-diameter,diameter,0.5*Math.PI,0,true);
+        ctx.arc(this.x, this.y - diameter, diameter, 0.5 * Math.PI, 0, true);
         ctx.fill();
         ctx.closePath();
 
         ctx.beginPath();
-        ctx.moveTo(this.boatX,  this.topPositionY-this.cameraY- this.height);
-        ctx.lineTo(this.x+lineWidth, this.y-this.height);
+        ctx.moveTo(this.boatX, this.topPositionY - this.cameraY - this.height);
+        ctx.lineTo(this.x + lineWidth, this.y - this.height);
         ctx.lineWidth = lineWidth;
         ctx.stroke();
         ctx.closePath();
@@ -59,7 +59,14 @@ var Hook = (function () {
 
     Hook.prototype.addFish = function (fish) {
         fish.angle = 1.5708;
-        this.fishes.push(fish);
+        if (this.fishes.length) {
+            var lastFish = this.fishes[this.fishes.length - 1];
+            if (lastFish.width < fish)
+                this.fishes.push(fish);
+
+        } else {
+            this.fishes.push(fish);
+        }
         this.goDown = false;
 
         GameModel.getInstance().score++;
@@ -96,7 +103,7 @@ var Hook = (function () {
             }
             else {
 
-                if(this.goDown==false){
+                if (this.goDown == false) {
                     var event = new Event(GameEvent.HOOK_ON_TOP);
                     dispatchEvent(event);
                 }
--- js/tutorial/TutorialManager.js
+++ js/tutorial/TutorialManager.js
@@ -4,7 +4,14 @@ var TutorialManager = (function () {
     function TutorialManager() {
         this.popup = new TutorialPopup();
         GameModel.getInstance().engine.elementsToDraw.push(this.popup);
-        this.popup.show("Tap to cast the line");
+        this.isMobile = GameModel.getInstance().device.isMobile;
+
+        var introTxt = "Click to cast the line";
+        if(this.isMobile){
+            introTxt = "Tap to cast the line";
+        }
+
+        this.popup.show(introTxt);
 
         addEventListener(GameEvent.CLOSE_POPUP, this.nextStep.bind(this));
         addEventListener(GameEvent.FISH_CAUGHT, this.nextStep.bind(this));
@@ -12,19 +19,22 @@ var TutorialManager = (function () {
     }
 
     TutorialManager.prototype.nextStep = function () {
-        if (tutorialStep > 3) return;
-        if (tutorialStep === 3) {
+        tutorialStep++;
+        if (tutorialStep > 4) return;
+        if (tutorialStep === 4) {
             var event = new Event(GameEvent.CLOSE_POPUP);
             dispatchEvent(event);
             return;
         }
-        tutorialStep++;
         var text1;
         var text2;
         var delay = 1000;
         switch (tutorialStep) {
             case 2:
                 text1 = "Move mouse to avoid fish";
+                if(this.isMobile){
+                    text1 = "Touch and move to avoid fish";
+                }
                 text2 = "On the way down";
                 removeEventListener(GameEvent.CLOSE_POPUP, this.nextStep.bind(this));
 
